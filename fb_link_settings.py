4da27f75454cfe4edb51930816a5ae75986693f4
richardwolff <40871448+richardwolff@users.noreply.github.com>
2020-01-11 14:52:49 -0800

    Create fb_link_settings.py

---
+++ fb_link_settings.py
@@ -0,0 +1,18 @@
+## your Facebook username and password, used by fbchat
+FACEBOOK_USERNAME = 
+FACEBOOK_PASSWORD = 
+
+## local path of OAuth2 credentials used by pygsheets
+LOCAL_PATH = 
+
+## list of name(s) of people in chat (e.g. ['John Smith','Jane Doe'])
+NAMES = 
+
+## thread_id of relevant Facebook chat
+THREAD_ID = 
+
+## name of sheet links will be saved to
+SHEETNAME = 
+
+## email address associated with Google sheets 
+EMAIL = 
