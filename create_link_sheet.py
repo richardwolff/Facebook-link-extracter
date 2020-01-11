c6d3ac680232da6196590aa6129868171d2da174
richardwolff <40871448+richardwolff@users.noreply.github.com>
2020-01-11 14:58:29 -0800

    Update create_link_sheet.py

--- create_link_sheet.py
+++ create_link_sheet.py
@@ -2,24 +2,21 @@ from fbchat import Client
 from fbchat.models import *
 import pandas as pd
 import pygsheets
-import fb_link_settings
+import fb_link_settings as settings
 
-## Facebook login: Client(username, password)
-client = Client( , )
+client = Client(settings.FACEBOOK_USERNAME, settings.FACEBOOK_PASSWORD)
 
-## insert path to local gsheetpermit json
-localpath=' ' 
+localpath = settings.LOCAL_PATH 
 
 namedict={}
 
-## insert names of people in group 
-names = []
+names = settings.NAMES
 
 for name in names:
   namedict[client.searchForUsers(name)[0].uid] = name
 
 ## insert thread_id here. Fetches last 10000 messages — adjust as necessary
-messages=client.fetchThreadMessages(thread_id=' ',,limit=10000)
+messages=client.fetchThreadMessages(thread_id=settings.THREAD_ID,limit=10000)
 
 lines=[]
 cols=['Name','Title','Link','Source']
@@ -42,9 +39,9 @@ for message in messages:
 c=pygsheets.authorize(service_file=localpath)
 
 ## insert name of gsheet to create and write to
-sh=c.create(' ')
+sh=c.create(settings.SHEETNAME)
 
 ## insert email associated with gsheets
-sh.share(" ",role='writer')
+sh.share(settings.EMAIL,role='writer')
 
 wks.set_dataframe(df1,(1,1))
