3d8f9dd44173861dae7fb3c7936cfcf9631f1726
richardwolff <40871448+richardwolff@users.noreply.github.com>
2020-01-11 17:19:59 -0800

    Update fb_link_settings.py

--- fb_link_settings.py
+++ fb_link_settings.py
@@ -1,3 +1,5 @@
+## All variables must be entered as strings, with ' ' around input. E.g. THREAD_ID = '123456789'.
+
 ## your Facebook username and password, used by fbchat
 FACEBOOK_USERNAME = 
 FACEBOOK_PASSWORD = 
@@ -8,7 +10,7 @@ LOCAL_PATH =
 ## list of name(s) of people in chat (e.g. ['John Smith','Jane Doe'])
 NAMES = 
 
-## thread_id of relevant Facebook chat
+## thread_id of relevant Facebook chat. see fbchat module documentation for more information.
 THREAD_ID = 
 
 ## name of sheet links will be saved to
